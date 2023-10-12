af8d75c9d102615529f35424723dc3ce02a04d44
pradiptechie <kumarpradip3956@gmail.com>
2023-10-12 16:01:46 +0545

    1st

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log("Hi there");
+const sharp = require ('sharp');
+
+// sharp('1.jpg')
+// .resize( 200, 200)
+// .toFile('out.jpg', (err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Resized succesfully");
+//     }
+// })
+
+const path = require('path');
+const multer = require('multer');
+const express =  require('express');
+const app = new express;
+
+app.set("view engine", "hbs");
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/img",(req,res)=>{
+    res.render("index");
+})
+
+
+///uploading file from frontend
+// const upload = multer({dest: "uploads/" });
+// app.post("/upload", upload.single("image"), (req,res)=>{
+//     res.end("Success uploading");
+// });
+
+
+// Configure Multer to use memory storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+app.post('/upload', upload.single('image'), (req, res) => {
+  // req.file contains the uploaded image as a buffer
+  const imageBuffer = req.file.buffer;
+  sharp(imageBuffer)
+  .resize(200, 200)
+  .toBuffer()
+  .then((processedImage) => {
+    const imageBase64 = processedImage.toString('base64');
+    const dataUrl = `data:image/jpeg;base64,${imageBase64}`;
+    res.send(dataUrl);
+  })
+  .catch((err) => {
+    res.status(500).send('Image processing error');
+  });
+
+
+        // .toFile('out.jpg', (err,data)=>{
+        //     if(err){
+        //         console.log(err)
+        //     }else{
+        //         console.log("Resized succesfully");
+        //     }
+        // })
+
+
+});
+
+
+
+
+app.listen((4500),()=>{
+    console.log("server running on port 4500");
+});
